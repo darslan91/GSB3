cf114860fd84dd5efb40b11ec7d092f82321b42b
tfoulley <tfoulley>
2019-01-17 08:58:37 +0100

    17/01/2019
    vue du haut et bas non connecté

---
+++ codeIgnit/application/views/non_connecte/v_haut.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->helper('url');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>La vue</title>
+		
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+	
+	</head>
+	<body>
