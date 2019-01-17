c23e90572436a648e5914b6c239f1fad71c2233d
tfoulley <tfoulley>
2019-01-17 09:05:56 +0100

    17/01/2019
    menu partie html

---
+++ codeIgnit/application/views/non_connecte/v_menu.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div class="vertical-menu">
+	<a href="" class="active">Accueil</a>
+	<a href="">lien 1</a>
+	<a href="">Lien 2</a>
+	<a href="">Lien 3</a>
+	<a href="">Lien 4</a>
+</div>
