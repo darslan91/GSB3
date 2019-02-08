f1f3ebd16c26f6542cd1702417b32bf626dc1e1f
Foulley <43532110+Foulley@users.noreply.github.com>
2019-02-08 09:24:29 +0100

    08/02/2019
    Navigation dans les pages fonctionnel
    -reste à faire la génération du nombre de page suivant une limite qui sera déterminer par l'utilisateur
    -afficher les praticien associé à leur page

--- codeIgnit/application/views/connecte/praticien/v_nb-page.php
+++ codeIgnit/application/views/connecte/praticien/v_nb-page.php
@@ -4,4 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php foreach ($nb as $key){
 	echo $key->nb;
+	echo br();
+	echo $key->nb/10;
+	echo br();
+	echo ceil($key->nb/10);
+	echo br();
+?>
+	<center>
+	<?php
+	for ($i=1; $i <= ceil($key->nb/10); $i++) {
+		echo anchor('c_praticien/index/'.$i.'/','Page '.$i, 'class="pagination"');
+	}
+	?>
+	</center>
+	<?php
 } ?>
