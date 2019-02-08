ada7517339a39354b26ba036a3a80e08df511dae
Foulley <43532110+Foulley@users.noreply.github.com>
2019-02-08 12:45:06 +0100

    08/02/2019
    Suppression partie inutile réalisé en double

--- codeIgnit/application/views/connecte/praticien/v_menu_praticien.php
+++
@@ -1,8 +0,0 @@
-<!-- /* --------------------------------------------------- */
-//Menu interne au compte rendu
-/* --------------------------------------------------- */ -->
-<div class="marg3">
-    <button class="tablink"><?php echo anchor('c_praticien/index','Praticien','class="tablink"');?></button>
-    <button class="tablink"><?php echo anchor('c_praticien/nouveau','Nouveau','class="tablink"');?></button>
-</div>
-<!-- /* --------------------------------------------------- */ -->
