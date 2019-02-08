cd9a6796d6e882fa1bcda1dd56ed4ebe64dffc4a
Foulley <43532110+Foulley@users.noreply.github.com>
2019-02-08 21:38:03 +0100

    08/02/2019
    -Limite fonctionnel en fonction du formulaire
    -mise en place de la recherche dans le même fichier
    ->flemme de faire séparément
    ->Rajoute de la difficulter de traitement de donné donc j'aime

--- codeIgnit/application/views/connecte/praticien/v_nb-page.php
+++ codeIgnit/application/views/connecte/praticien/v_nb-page.php
@@ -2,30 +2,55 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-echo form_open('c_compte/rechercheImpressise', $_POST);
+echo form_open('c_praticien/index/1/15', $_POST);
 ?>
-	<select name="nom">
-		<option value="indef">Indefinie</option>
-		<?php foreach ($praticien as $key): ?>
-			<option value="<?php ?>"></option>
-		<?php endforeach ?>
-	</select>
-	<select name="cp">
-		<option value="indef">Indefinie</option>
-		<?php foreach ($praticien as $key): ?>
-			<option value="<?php ?>"></option>
-		<?php endforeach ?>
-	</select>
-
-<?php foreach ($nb as $key){
-	echo br();
-	echo "Page numero: ".$num;
-	echo br();
-	echo "Valeur pra_num haut: ".$btnHaut;
-	echo br();
-	echo "Valeur pra_num bas: ".$btnBas;
-	echo br();
-?>
+<table style="border: 1px solid grey; margin-bottom: 2px;">
+	<tr>
+		<th style="border: 1px solid grey;">Nom</th>
+		<th style="border: 1px solid grey;">Localisation</th>
+		<th style="border: 1px solid grey;">Limite</th>
+		<th style="border: 1px solid grey;">Valider</th>
+	</tr>
+	<tr>
+		<td style="border: 1px solid grey;">
+			<select name="nom">
+				<option value="indef">Indefinie</option>
+				<?php foreach ($praticien as $key): ?>
+					<option value="<?php ?>"></option>
+				<?php endforeach ?>
+			</select>
+		</td>
+		<td style="border: 1px solid grey;">
+			<select name="cp">
+				<option value="indef">Indefinie</option>
+				<?php foreach ($praticien as $key): ?>
+					<option value="<?php ?>"></option>
+				<?php endforeach ?>
+			</select>
+		</td>
+		<td style="border: 1px solid grey;">
+			<select name="limite">
+				<option>10</option>
+				<option>15</option>
+				<option>20</option>
+				<option>25</option>
+				<option>30</option>
+				<option>35</option>
+				<option>40</option>
+				<option>45</option>
+				<option>50</option>
+			</select>
+		</td>
+		<td style="border: 1px solid grey;">
+			<?php
+			    echo form_submit('envoie', 'Envoyer');
+			    echo form_close();
+			?>
+		</td>
+	</tr>
+</table>
+	
+<?php foreach ($nb as $key){ ?>
 	<center>
 	<?php
 	for ($i=1; $i <= ceil($key->nb/$limit); $i++) {
@@ -41,3 +66,14 @@ echo form_open('c_compte/rechercheImpressise', $_POST);
 	<?php
 } ?>
 </footer>
+<?php foreach ($nb as $key){
+	echo br();
+	echo br();
+	echo "Page numero: ".$num;
+	echo br();
+	echo "Valeur pra_num haut: ".$btnHaut;
+	echo br();
+	echo "Valeur pra_num bas: ".$btnBas;
+	echo br();
+}
+?>
