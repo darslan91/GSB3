e64285df1431e24266d6c346fe6a73bbd7a60178
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-02-19 15:54:32 +0100

    19/02/2019
    /* ---------- VUES ---------- */
    - Vu tableau pour les medicaments
    
    /* ---------- MODELE ---------- */
    - Suppression de la limite
    
    REMARQUE  ->
    - Essayer de rendre le tableau le contenue scrollable

--- codeIgnit/application/models/modele_deniz.php
+++ codeIgnit/application/models/modele_deniz.php
@@ -13,7 +13,7 @@ class modele_deniz extends CI_Model{
      * Cette fonction permet de récuperer tout les médicaments contenue dans la tables médicament
      */
     public function getLesMedicaments(){
-        $req="SELECT med_depotlegal,med_nomcommercial,fam_code,med_composition,med_effets,med_contreindic,med_prixechantillon AS prix FROM medicament ORDER BY med_depotlegal LIMIT 5";
+        $req="SELECT med_depotlegal,med_nomcommercial,fam_code,med_composition,med_effets,med_contreindic,med_prixechantillon AS prix FROM medicament ORDER BY med_depotlegal";
         $query=$this->db->query($req)->result();
         
         return $query;
