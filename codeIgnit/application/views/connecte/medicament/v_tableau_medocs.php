19344ce65983a6a96bd3ccea7ba408b57fb8d939
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-02-25 19:13:48 +0100

    25/02/2019
    /* ---------- VUE ---------- */
    - Essayer de faire juste scroller le tableau sans la div

--- codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
+++ codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
@@ -2,35 +2,48 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<!-- TABLEAUX MEDICAMENTS -->
-<div style="max-height: 400px; overflow: auto;">
-    <table class="table">
-        <tr class="table">
-            <th>Dépôt Légal</th>
-            <th>Nom Commercial</th>
-            <th>Code Famille Médicament</th>
-            <th>Médicament Composition</th>
-            <!-- <th>Médicament Effets</th> -->
-            <!-- <th>Médicament Contre-indications</th> -->
-            <!-- <th>Prix Echantillon</th> -->
-        </tr>
-        
-        <!-- Début tableau -->
-        <?php 
-        foreach ($medicament as $key){
-        ?>
+<style>
+    #table-scroll{
+        display: inline-block;
+        overflow: auto;
+        height: 40px;
+    }
+</style>
 
-        <tr class="table">
-            <td><?php echo $key->med_depotlegal?></td>
-            <td><?php echo $key->med_nomcommercial?></td>
-            <td><?php echo $key->fam_code?></td>
-            <td><?php echo $key->med_composition?></td>
-            <!-- <td><?php echo $key->med_effets?></td> -->
-            <!-- <td><?php echo $key->med_contreindic?></td> -->
-            <!-- <td><?php echo $key->prix?></td> -->
-        </tr>
-        <?php
-        }
-        ?>
-    </table>
-</div>
+<!-- TABLEAUX MEDICAMENTS -->
+
+<table class="table">
+    <tr class="table">
+        <th>Dépôt Légal</th>
+        <th>Nom Commercial</th>
+        <th>Code Famille Médicament</th>
+        <th>Médicament Composition</th>
+        <!-- <th>Médicament Effets</th> -->
+        <!-- <th>Médicament Contre-indications</th> -->
+        <!-- <th>Prix Echantillon</th> -->
+    </tr>
+        
+    <!-- Début tableau -->
+    <?php 
+    foreach ($medicament as $key){
+    ?>
+
+    <tr class="table">
+        <td colspan="4">
+            <div style="overflow: auto;">    
+                <table>
+                    <tr>
+                        <td><?php echo $key->med_depotlegal?></td>
+                        <td><?php echo $key->med_nomcommercial?></td>
+                        <td><?php echo $key->fam_code?></td>
+                        <td><?php echo $key->med_composition?></td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+
+    <?php
+    }
+    ?>
+</table>
