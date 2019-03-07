685aded3840033de2f9e9a2a5a75ce9bdb1b31cc
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-03-07 18:42:45 +0100

    07/03/2019
    /* ----------- VUE ----------- */
    - Création de la vue praticiens

---
+++ codeIgnit/application/views/connecte/praticien/v_ajout_praticien.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!-- /* --------------------------------------------------- */
+//Formulaire pour ajouté un nouveau praticien
+/* --------------------------------------------------- */ -->
+<h3>Nouveau particien</h3>
+<div style="overflow-x:auto;" class="compte-rendu">
+    <table class="table">
+        <tr class="table">
+            <th>Nom</th>
+            <th>Prénom</th>
+        </tr>
+
+        <?php echo form_open('c_praticien/');?>
+        <tr>
+        </tr>
+    </table>
+</div>
