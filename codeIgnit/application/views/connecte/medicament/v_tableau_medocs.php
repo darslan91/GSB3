d3172a9139221248105a288c0e5e13e66c51334e
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-03-09 11:24:07 +0100

    09/03/2019
    /* ------------ VUE ---------- */
    - Le style est améliorer mais reste des changement a faire.

--- codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
+++ codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
@@ -5,15 +5,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <style>
     table{
         border-collapse: collapse;
+        border-spacing: 0;
+        width: 100%;
+        border: 1px solid grey;
     }
 
-    thead, tfoot{
-        background-color: plum;
+    .table th, td{
+        text-align: left;
+        padding: 8px;
     }
 
-    tbody{
-        background-color: pink;
+    .table th{
+    background-color: #D8D8D8;
     }
+
+    .table tr:nth-child(even){background-color: white}
 
     #tableau-neutre{
         border: 1px solid black;
@@ -30,35 +36,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <h2> Tableau des médicaments </h2>
 
-<table id="tableau-neutre">
-    <thead>
-        <tr>
-            <th>Dépôt Légal</th>
-            <th>Nom Commercial</th>
-            <th>Code Famille Médicament</th>
-            <th>Détails</th>
-            <th> </th><!-- Caractère invisible avec alt+255 -->
-            <th> </th><!-- Caractère invisible avec alt+255 -->
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td colspan="6"><!-- Adapter le nombre en fonction du nombre de <th> au dessus -->
-                <table id="tableau-scroll">
-                    <?php 
-                    foreach ($medicament as $key){
-                    ?>
-                    <tr>
-                        <td><?php echo $key->med_depotlegal;?></td>
-                        <td><?php echo $key->med_nomcommercial;?></td>
-                        <td><?php echo $key->fam_code;?></td>
-                        <td><a href="detail/<?php echo $key->med_depotlegal;?>" target="wclose" onclick="window.open('detail/<?php echo $key->med_depotlegal;?>','wclose', 'width=500,height=700,toolbar=no,status=no,left=60,top=110')">Plus</a></td>
-                    </tr>
-                    <?php
-                    }
-                    ?>
-                </table>
-            </td>
-        </tr>
-    </tbody>
+<table class="table">
+    <tr class="table">
+        <th>Dépôt Légal</th>
+        <th>Nom Commercial</th>
+        <th>Code Famille Médicament</th>
+        <th>Détail</th>
+    </tr>
+
+    <tr>
+        <td colspan="4">
+            <table id="tableau-scroll">
+                <?php
+                foreach($medicament as $key){ 
+                ?>
+                <tr>
+                    <td><?php echo $key->med_depotlegal;?></td>
+                    <td><?php echo $key->med_nomcommercial;?></td>
+                    <td><?php echo $key->fam_code;?></td>
+                    <td><a href="detail/<?php echo $key->med_depotlegal;?>" target="wclose" onclick="window.open('detail/<?php echo $key->med_depotlegal;?>','wclose', 'width=500,height=700,toolbar=no,status=no,left=60,top=110')">Plus</a></td>
+                </tr>
+                <?php
+                } 
+                ?>
+            </table>
+        </td>
+    </tr>
 </table>
