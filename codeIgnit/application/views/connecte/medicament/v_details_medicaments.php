d065f8b297f83a9d88b8d8aabf4b4e40fcfa39d3
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-03-10 17:59:45 +0100

    10/03/2019
    /* ---------- VUE ---------- */
    - Amélioration du style pour la vue des détails du médicament

--- codeIgnit/application/views/connecte/medicament/v_details_medicaments.php
+++ codeIgnit/application/views/connecte/medicament/v_details_medicaments.php
@@ -4,48 +4,60 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <style>
     h1{
         text-align: center;
+        font-family: normal Helvetica, Arial, sans-serif;
     }
+    .font{
+        font-family: normal Helvetica, Arial, sans-serif;
+        font-size : 20px;
+        text-decoration: underline #003399;
+    }
+
+    .font_result{
+        font-family: normal Helvetica, Arial, sans-serif;
+        font-size : 15px;
+        color: #4F5155;
+    } 
 </style>
 
 
 
-<h1>Information sur le médicament</h1>
+<h1>Informations sur le médicament</h1>
 
 <p>
-    <h3>Dépôt Légal : </h3>
-    <?php echo $medicament[0]->med_depotlegal;?>
+    <h3 class="font">Dépôt Légal : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->med_depotlegal."</h3>";?>
 </p>
 
 <p>
-    <h3>Nom Commercial : </h3>
-    <?php echo $medicament[0]->med_nomcommercial;?>
+    <h3 class="font">Nom Commercial : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->med_nomcommercial."</h3>";?>
 </p>
 
 <p>
-    <h3>Code Famille Medicament : </h3>
-    <?php echo $medicament[0]->fam_code;?>
-    <h3>Libellé Famille Médicament : </h3>
-    <?php echo $medicament[0]->fam_libelle;?>
+    <h3 class="font">Code Famille Medicament : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->fam_code."</h3>";?>
+    <h3 class="font">Libellé Famille Médicament : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->fam_libelle."</h3>";?>
 </p>
 
 <p>
-    <h3>Composition du Médicament : </h3>
-    <?php echo $medicament[0]->med_composition;?>
+    <h3 class="font">Composition du Médicament : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->med_composition."</h3>";?>
 </p>
 
 <p>
-    <h3>Effet du Médicament : </h3>
-    <?php echo $medicament[0]->med_effets;?>
+    <h3 class="font">Effet du Médicament : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->med_effets."</h3>";?>
 </p>
 
 <p>
-    <h3>Contre-indications du Médicament : </h3>
-    <?php echo $medicament[0]->med_contreindic;?>
+    <h3 class="font">Contre-indications du Médicament : </h3>
+    <?php echo "<h3 class=\"font_result\">".$medicament[0]->med_contreindic."</h3>";?>
 </p>
 
 <form method="post">
     <p align="center">
         <input type="button" value="Fermer la fenêtre" onClick="window.close()">
-        <input type="button" value="Format PDF"/>
+        <input type="button" value="Format PDF" href="connectee/pdf/pdf_detail_medicament.php"/>
     </p>
 </form>	
