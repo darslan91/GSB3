b95fee64909cda74d6e74ca304b6338119e0211e
deniz <deniz.arslan.pro@gmail.com>
2019-03-12 13:15:04 +0100

    12/03/2018
    /* ---------- VUE ---------- */
    - Modification de la taille de la fenêtre

--- codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
+++ codeIgnit/application/views/connecte/medicament/v_tableau_medocs.php
@@ -54,7 +54,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <td><?php echo $key->med_depotlegal;?></td>
                     <td><?php echo $key->med_nomcommercial;?></td>
                     <td><?php echo $key->fam_code;?></td>
-                    <td><a href="detail/<?php echo $key->med_depotlegal;?>" target="wclose" onclick="window.open('detail/<?php echo $key->med_depotlegal;?>','wclose', 'width=500,height=700,toolbar=no,status=no,left=60,top=110')">Plus</a></td>
+                    <td><a href="detail/<?php echo $key->med_depotlegal;?>" target="wclose" onclick="window.open('detail/<?php echo $key->med_depotlegal;?>','wclose', 'width=500,height=825,toolbar=no,status=no,left=60,top=110')">Plus</a></td>
                 </tr>
                 <?php
                 } 
