f9674959ec8a3fbcedb68a6961729074817b9215
darslan91 <43532168+darslan91@users.noreply.github.com>
2019-03-14 01:02:29 +0100

    14/03/2019
    /* ----------- PDF ---------- */
    - Fin du PDF toujours un problème avec le logo
    
    /* -------------------------- */
    ---------- THE END ----------
    /* -------------------------- */

--- codeIgnit/application/views/connecte/pdf/pdf_detail_medicament.php
+++ codeIgnit/application/views/connecte/pdf/pdf_detail_medicament.php
@@ -7,13 +7,13 @@ class PDF extends FPDF{
     //En tête
     function Header(){
         //Logo
-        //$this->Image('logo.jpg', 10, 6, 30);
+        //$this->Image('logo.jpg',null, null, 6,30,'JPG');
         //Police
         $this->SetFont('Arial','B',15);
         //Décalage à droite
         $this->Cell(80);
 		// Titre
-		$this->Cell(30,10,'Fiche Detail du Medicament',0,0,'C');
+		$this->Cell(30,10,utf8_decode('Fiche Détail du Médicament'),0,0,'C');
 		// Saut de ligne
 		$this->Ln(20);
 
@@ -39,21 +39,21 @@ $pdf->SetFont('Arial','',12);
 
 //Titre
 $pdf->SetFont('Arial','B',13);
-$pdf->Cell(10,10,'Detail du medicament',0,0);
+$pdf->Cell(10,10,utf8_decode('Détail du médicament'),0,0);
 $pdf->Ln(15);
 
 
 /* TABLEAUX DU MEDICAMENT */
 $pdf->SetFont('Arial','B',12);
-$pdf->Cell(10,10,'Le medicament',0,0);
+$pdf->Cell(10,10,utf8_decode('Le médicament'),0,0);
 $pdf->SetFont('Arial','',12);
 $pdf->Ln(10);
 //Le med
-$pdf->Cell(35,10,'Depot Legal',1,0,'C');
-$pdf->Cell(35,10,'Nom Commercial',1,0,'C');
+$pdf->Cell(35,10,utf8_decode('Dépôt Légal'),1,0,'C');
+$pdf->Cell(50,10,'Nom Commercial',1,0,'C');
 $pdf->Ln();
 $pdf->Cell(35,10,utf8_decode($medicament['med_depotlegal']),1,0,'C');
-$pdf->Cell(35,10,utf8_decode($medicament['med_nomcommercial']),1,0, 'C');
+$pdf->Cell(50,10,utf8_decode($medicament['med_nomcommercial']),1,0, 'C');
 $pdf->Ln(15);
 
 $pdf->SetFont('Arial','B',12);
@@ -63,31 +63,38 @@ $pdf->Ln(10);
 
 //Sa famille
 $pdf->Cell(35,10,'Code Famille',1,0,'C');
-$pdf->Cell(60,10,'Libelle Famille',1,0,'C');
+$pdf->Cell(120,10,utf8_decode('Libellé Famille'),1,0,'C');
 $pdf->Ln();
 $pdf->Cell(35,10,utf8_decode($medicament['fam_code']),1,0,'C');
-$pdf->Cell(60,10,utf8_decode($medicament['fam_libelle']),1,0,'C');
+$pdf->Cell(120,10,utf8_decode($medicament['fam_libelle']),1,0,'C');
 $pdf->Ln(15);
 
 $pdf->SetFont('Arial','B',12);
-$pdf->Cell(10,10,'Detail composition',0,0);
+$pdf->Cell(10,10,utf8_decode('Détails compositions'),0,0);
 $pdf->SetFont('Arial','',12);
 $pdf->Ln(10);
+
 //Detail composition
+$pdf->SetFont('Arial','U',12);
 $pdf->Cell(35,10,'Composition : ',0,0);
-$pdf->Cell(80,10,utf8_decode($medicament['med_composition']),0,0,'C');
-$pdf->Ln();
-
-$pdf->Cell(35,10,'Effet : ',0,0);
-$pdf->Cell(150,10,utf8_decode($medicament['med_effets']),0,0,'C');
-$pdf->Ln();
-$pdf->Cell(50,10,"",0,0);
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(100,10,utf8_decode($medicament['med_composition']),0,0,'C');
 
 $pdf->Ln();
+$pdf->Ln();
+
+$pdf->SetFont('Arial','U',12);
+$pdf->Cell(35,10,'Effets : ',0,0);
+$pdf->SetFont('Arial','',12);
+$pdf->Ln();
+$pdf->MultiCell(150, 10, utf8_decode($medicament['med_effets']),0,'J');
+
+$pdf->Ln();
+$pdf->SetFont('Arial','U',12);
 $pdf->Cell(37,10,'Contre-indications :',0,0);
+$pdf->SetFont('Arial','',12);
 $pdf->Ln();
-$pdf->Cell(60,10,"",0,0);
-$pdf->Cell(80,10,utf8_decode($medicament['med_contreindic']),0,0,'C');
+$pdf->MultiCell(150, 10, utf8_decode($medicament['med_contreindic']),0,'J');
 
 //Ouverture du pdf normalement
 $pdf->Output();
